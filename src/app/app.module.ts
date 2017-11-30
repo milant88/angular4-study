3d05ee928b1e97533dd8b663beae0662dd9f7b85
milant88 <milan.tomic@computerrock.com>
2017-11-30 11:08:00 +0100

    core components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 
 @NgModule({
   declarations: [
-      AppComponent
+      AppComponent,
+      HeaderComponent,
+      RecipesComponent,
+      RecipeListComponent,
+      RecipeDetailComponent,
+      RecipeItemComponent,
+      ShopingListComponent,
+      ShopingEditComponent
   ],
   imports: [
       BrowserModule,
